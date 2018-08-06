b9a6632fdc46ad63dbbcec8b3fe548ea04eb29fc
SELIM REZA <SELIMREZAEL@GMAIL.com>
2018-08-06 09:29:05 +0530

    added more details

--- index.js
+++ index.js
@@ -2,31 +2,49 @@ const express = require('express');
 const cheerio = require('cheerio');
 const app = express();
 const rp = require('request-promise');
+const baseUrl = 'http://mycricket.cricket.com.au'
 
-const options = {
-  uri: `http://mycricket.cricket.com.au/pages/quickentitysearch.aspx?searchText=South%20Perth%20Junior%20Cricket%20Club`,
-  transform: function (body) {
-    return cheerio.load(body);
-  }
-};
+//
+// const options1 = {
+//   uri: `http://mycricket.cricket.com.au/pages/quickentitysearch.aspx?searchText=Balmain%20South%20Sydney%20Cricket%20Club`,
+//   transform: function (body) {
+//     return cheerio.load(body);
+//   }
+// };
+//
+// const options2 = {
+//   uri: `http://mycricket.cricket.com.au/pages/quickentitysearch.aspx?searchText=Balmain%20South%20Sydney%20Cricket%20Club`,
+//   transform: function (body) {
+//     return cheerio.load(body);
+//   }
+// };
 
-var arr= []
+const generateOptions = (url) => {
+ return {
+   uri: url,
+   transform: function (body) {
+     return cheerio.load(body);
+   }
+ }
+}
+
 
 app.get('/scrape', function(req, res){
+  var options = generateOptions(`${baseUrl}/pages/quickentitysearch.aspx?searchText=Balmain%20South%20Sydney%20Cricket%20Club`)
   rp(options)
   .then(($) => {
     var results = $('#club-results-container').find('table.search-table tbody tr.RVDataGridItem');
-    // var i;
-    // for (i = 0; i < results.length; i++) { 
-      
-    // }
+    var arr = []
 
     results.each(function(i, elem) {
       var currentRow = $(this);
-
-      // console.log(currentRow.find("td").eq(0).text());
+      var entityID = currentRow.find("td").eq(3).find('a[id=dgClubResults_ctl03_lnkView]').attr('href').match(/[0-9]\w+/)
       arr.push({
-        col1_value: currentRow.find("td").eq(0).text()
+        clubName: currentRow.find("td").eq(0).text(),
+        facebookUrl: currentRow.find("td").eq(1).find('a[title=Facebook]').attr('href'),
+        twitterURl: currentRow.find("td").eq(1).find('a[title=Twitter]').attr('href'),
+        state: currentRow.find("td").eq(2).text(),
+        clubDetails: clubDetails(entityID)
       })
     })
     res.send(arr);
@@ -36,6 +54,17 @@ app.get('/scrape', function(req, res){
   });
 })
 
+
+var clubDetails = (entityID) => {
+  var options = generateOptions(`${baseUrl}/common/pages/public/entitydetails.aspx?save=0&entityID=${entityID}`);
+  rp(options)
+  .then(($) => {
+    var arra = []
+    // var results =  $('#club_details #ed1_DataGridMain table tr td.tblRuleLight');
+    arra.push({test: 'test'})
+  })
+}
+
 app.listen('8081')
 console.log('Running on port 8081');
 exports = module.exports = app;
@@ -43,6 +72,7 @@ exports = module.exports = app;
 
 
 // http://mycricket.cricket.com.au/common/pages/public/entitydetails.aspx?entityID=2413
+// http://mycricket.cricket.com.au/common/pages/public/entitydetails.aspx?save=0&entityID=3406
 // http://mycricket.cricket.com.au/home.aspx?entityID=2413
 
 // result.each(function(i, td){
---
+++ notes.js
@@ -0,0 +1,83 @@
+CLub
+====
+
+name: ,
+shortname
+website_url:
+address:
+facebook:
+conntact: {
+  firstname:
+  lastname:
+  address:
+  email:
+  phone:
+},
+background_club_image:
+primary_colour:
+secondary_colour:
+is_stripped:
+tshirt_colour:
+stripe_colour:
+sleeve_color:
+logo:
+number_of_teams:
+payment_date:
+paid:
+source_of_association_data:
+trial_start_date:
+trial_end_date:
+account_status:
+isFullPosition:
+timezone:
+----------
+Fixtures
+=======
+club_id:
+round_number:
+start_date_time:
+end_date_time:
+team_id:
+opposition_as: Home/away
+opposition_club_id:
+venue_id:
+image:
+---------
+
+Players
+=========
+club_id:
+	role_id:
+	player_id:
+	nickname:
+	firstname:
+	lastname:
+	email:
+	mobile:
+	profile_image:
+	dateOfBirth:
+	notes:
+	skills:
+	skillNames:
+
+	skill_positions:
+	gender:
+	footed:
+	squad_id:
+	last_contact: {
+		contact_status:
+		contact_date:
+	},
+	// specialist_role: {
+	// 	type: Schema.Types.ObjectId, ref: 'PlayerSpecialist'
+	// },
+	// batting_preference: {
+	// 	type: Schema.Types.ObjectId, ref: 'BattingPreference'
+	// },
+	// bowling_action: {
+	// 	type: Schema.Types.ObjectId, ref: 'BowlingAction'
+	// },
+	// bowling_type: {
+	// 	type: Schema.Types.ObjectId, ref: 'BowlingType'
+	// },
+	
